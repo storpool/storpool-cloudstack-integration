20f3feb0bea79101b236e5ad0c7cf68921658fbb
Slavka Peleva <slavkap@storpool.com>
2019-10-29 12:38:41 +0200

    Fix in listener for detach/attach volumes
    There was an issue with attach/detach listener with all volumes. Now it sends request only with StorPool volumes
    
    Change-Id: I901675b300d5b78e59689ecacecbe143649ed5ff

--- src/com/cloud/hypervisor/kvm/storage/StorpoolStorageListner.java
+++ src/com/cloud/hypervisor/kvm/storage/StorpoolStorageListner.java
@@ -11,31 +11,25 @@ import com.cloud.storage.Volume;
 public class StorpoolStorageListner {
     private static final Logger log = Logger.getLogger(StorpoolStorageListner.class);
 
-    @AfterReturning(pointcut = "execution (* com.cloud.storage.VolumeApiServiceImpl.attachVolumeToVM(..))", returning="retVal")
-    public void afterAttachVolumeToVM(Object retVal) {
-        log.info("VolumeApiServiceImpl.attachVolumeToVM");
-        log.info(String.format("VolumeApiServiceImpl.attachVolumeToVM obj=%s", retVal));
-        log.info(String.format("Object is Volume instance=%s ",retVal instanceof Volume));
-        if (retVal instanceof Volume) {
-           Volume vol = (Volume) retVal;
-           log.info(String.format("Volume id=%s, name=%s, instanceId=%s, path=%s", vol.getId(), vol.getName(),vol.getInstanceId(), vol.getPath()));
-           Long vmId = vol.getInstanceId();
-           String name = StorpoolStorageAdaptor.getVolumeNameFromPath(vol.getPath());
-           StorpoolUtil.volumeUpadateTags(name, vmId);
-       }
+    @AfterReturning(pointcut = "execution (* com.cloud.storage.VolumeApiServiceImpl.attachVolumeToVM(..)) && args(vmId, volumeId, deviceId)", returning="retVal")
+    public void afterAttachVolumeToVM(Object retVal, Long vmId, Long volumeId, Long deviceId) {
+        updateVolumeTags(retVal, vmId);
     }
 
     @AfterReturning(pointcut = "execution (* com.cloud.storage.VolumeApiServiceImpl.detachVolumeFromVM(..))", returning="retVal")
     public void afterDetachVolumeToVM(Object retVal) {
-        log.info("VolumeApiServiceImpl.attachVolumeToVM");
-        log.info(String.format("VolumeApiServiceImpl.attachVolumeToVM obj=%s", retVal));
-        log.info(String.format("Object is Volume instance=%s ",retVal instanceof Volume));
+        updateVolumeTags(retVal, null);
+    }
+
+    private void updateVolumeTags(Object retVal, Long vmId ) {
         if (retVal instanceof Volume) {
-           Volume vol = (Volume) retVal;
-           log.info(String.format("Volume id=%s, name=%s, instanceId=%s, path=%s", vol.getId(), vol.getName(),vol.getInstanceId(), vol.getPath()));
-           Long vmId = vol.getInstanceId();
-           String name = StorpoolStorageAdaptor.getVolumeNameFromPath(vol.getPath());
-           StorpoolUtil.volumeUpadateTags(name, vmId);
-       }
+            Volume vol = (Volume) retVal;
+            log.info(String.format("Volume id=%s, name=%s, instanceId=%s, path=%s", vol.getId(), vol.getName(),
+                    vol.getInstanceId(), vol.getPath()));
+            String name = StorpoolStorageAdaptor.getVolumeNameFromPath(vol.getPath());
+            if (name != null) {
+                StorpoolUtil.volumeUpadateTags(name, vmId);
+            }
+        }
     }
 }
