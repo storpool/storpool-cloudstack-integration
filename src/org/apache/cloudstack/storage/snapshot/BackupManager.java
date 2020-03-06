7153281a425c54e21e7bb780ae5357aa33274e08
Slavka Peleva <slavkap@storpool.com>
2020-03-06 17:11:37 +0200

    Fixed issue for download volume. Added more tests
    Fixed issue for Cast exception of DataStoreTO when downloading volume, getting StorPool's cluster id, because it's needed for attach snapshot command on agents.
    Added default value for sp.cluster.id setting, because if it's not set throws NPE.
    Added more tests for migrating VM/volumes from NFS to StorPool, testing snapshots whit secondary and bypassing secondary storage, download volume
    
    Change-Id: I89ed411a9f8883dea2adc1a75477b03f7f7c8723

--- src/org/apache/cloudstack/storage/snapshot/BackupManager.java
+++ src/org/apache/cloudstack/storage/snapshot/BackupManager.java
@@ -6,7 +6,7 @@ import org.apache.cloudstack.framework.config.Configurable;
 public class BackupManager implements Configurable{
     public static final ConfigKey<Boolean> BypassSecondaryStorage = new ConfigKey<Boolean>(Boolean.class, "sp.bypass.secondary.storage", "Advanced", "false",
             "For StorPool Managed storage backup to secodnary", true, ConfigKey.Scope.Global, null);
-    public static final ConfigKey<String> StorPoolClusterId = new ConfigKey<String>(String.class, "sp.cluster.id", "Advanced", null,
+    public static final ConfigKey<String> StorPoolClusterId = new ConfigKey<String>(String.class, "sp.cluster.id", "Advanced", "n/a",
                                     "For StorPool multi cluster authorization", true, ConfigKey.Scope.Cluster, null);
 
     @Override
