947781ab8211204d92874fd0c51d85065a8af4c9
Slavka Peleva <slavkap@storpool.com>
2022-01-20 08:47:40 +0200

    mgmt: Added API params on create tags command
    Change-Id: I270f2ceb07133503faa803b26d83bb96c9810128

--- src/org/apache/cloudstack/storage/helper/StorPoolCreateTagsCmd.java
+++ src/org/apache/cloudstack/storage/helper/StorPoolCreateTagsCmd.java
@@ -1,11 +1,14 @@
 package org.apache.cloudstack.storage.helper;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.apache.cloudstack.api.APICommand;
+import org.apache.cloudstack.api.ApiConstants;
 import org.apache.cloudstack.api.BaseAsyncCmd;
+import org.apache.cloudstack.api.Parameter;
 import org.apache.cloudstack.api.ServerApiException;
 import org.apache.cloudstack.api.command.user.tag.CreateTagsCmd;
 import org.apache.cloudstack.api.response.SuccessResponse;
@@ -31,6 +34,21 @@ import com.cloud.vm.dao.VMInstanceDao;
 requestHasSensitiveInfo = false, responseHasSensitiveInfo = false)
 public class StorPoolCreateTagsCmd extends BaseAsyncCmd{
     private static final Logger log = Logger.getLogger(StorPoolCreateTagsCmd.class);
+
+    @Parameter(name = ApiConstants.TAGS, type = CommandType.MAP, required = true, description = "Map of tags (key/value pairs)")
+    private Map tag;
+
+    @Parameter(name = ApiConstants.RESOURCE_TYPE, type = CommandType.STRING, required = true, description = "type of the resource")
+    private String resourceType;
+
+    @Parameter(name = ApiConstants.RESOURCE_IDS, type = CommandType.LIST, required = true, collectionType = CommandType.STRING,
+               description = "list of resources to create the tags for")
+    private List<String> resourceIds;
+
+    @Parameter(name = ApiConstants.CUSTOMER, type = CommandType.STRING, description = "identifies client specific tag. "
+        + "When the value is not null, the tag can't be used by cloudStack code internally")
+    private String customer;
+
     private CreateTagsCmd createTagsCmd;
     private StorPoolReplaceCommandsHelper.StorPoolReplaceCommandsUtil replaceCommands = StorPoolReplaceCommandsHelper.getStorPoolReplaceCommandsUtil();
     @Inject
